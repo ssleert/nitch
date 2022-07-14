b83384f42eb07876c31bffc1bbf08764e511f51a
sfome <semenilichevsky@gmail.com>
2022-07-15 01:57:44 +0300

    Fix: minor

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-wget "https://github.com/unxsh/nitch/releases/download/0.1.2/nitch"
+wget "https://github.com/unxsh/nitch/releases/download/0.1.3/nitch"
 chmod +x nitch
 sudo mv nitch /usr/local/bin
