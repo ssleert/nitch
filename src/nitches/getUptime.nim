e742e47f04177a05bfe30354ad247f333727cc72
sfome <semenilichevsky@gmail.com>
2022-07-16 01:29:28 +0300

    Fix: fix time len bug

--- src/nitches/getUptime.nim
+++ src/nitches/getUptime.nim
@@ -5,8 +5,6 @@ proc getUptime*(): string =
     uptimeSeq: seq[string] = readFile("/proc/uptime").split(".")
     uptimeHours: string = $(parseInt(uptimeSeq[0]) / 3600)
 
-  if len(uptimeHours) > 5:
-    result = uptimeHours[0 .. 4] & "h"
+    uptimeHoursSeq: seq[string] = uptimeHours.split(".")
 
-  else:
-    result = uptimeHours & "h"
+  result = uptimeHoursSeq[0] & "." & uptimeHoursSeq[1][0 .. 1] & "h"
