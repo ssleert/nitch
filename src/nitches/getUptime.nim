658b35fcf6b7a5f7f1730dc23e94bd12adf287a2
sfome <semenilichevsky@gmail.com>
2022-07-17 00:09:55 +0300

    Update: added normal time

--- src/nitches/getUptime.nim
+++ src/nitches/getUptime.nim
@@ -3,8 +3,7 @@ import std/strutils
 proc getUptime*(): string =
   let
     uptimeSeq: seq[string] = readFile("/proc/uptime").split(".")
-    uptimeHours: string = $(parseInt(uptimeSeq[0]) / 3600)
+    uptimeHours: string = $(parseInt(uptimeSeq[0]) div 3600)
+    uptimeMinutes: string = $(parseInt(uptimeSeq[0]) mod 3600 div 60)
 
-    uptimeHoursSeq: seq[string] = uptimeHours.split(".")
-
-  result = uptimeHoursSeq[0] & "." & uptimeHoursSeq[1][0 .. 1] & "h"
+  result = uptimeHours & "h " & uptimeMinutes & "m"
