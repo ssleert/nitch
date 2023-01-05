d9243a1e4d64f0486a9b68813b01f36d0e567cf3
Dou2ble <22otro@stockholmscience.se>
2023-01-05 23:58:49 +0100

    added pacman execution for artix

--- src/nitches/getPkgs.nim
+++ src/nitches/getPkgs.nim
@@ -7,6 +7,9 @@ proc getPkgs*(distroId: string): string =
   case distroId:
   of "arch":
     result = getPacmanPkgs()
+  
+  of "artix":
+    result = getPacmanPkgs()
 
   of "archcraft":
     result = getPacmanPkgs()
